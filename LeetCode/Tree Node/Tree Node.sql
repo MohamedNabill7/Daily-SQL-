a2146666d9b1779e12b72da9a7bc41a93ae18a8c
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-06-07 00:04:19 +0200

    Solution

---
+++ LeetCode/Tree Node/Tree Node.sql
@@ -0,0 +1,10 @@
+select 
+        id ,
+        case
+                when p_id is null then 'Root'
+                when (id not in (select distinct(p_id) from tree where p_id is not null)) then 'Leaf'
+                else 'Inner'
+        end as type
+from
+        Tree 
+        order by id
