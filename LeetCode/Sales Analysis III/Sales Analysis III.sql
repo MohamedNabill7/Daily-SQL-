eccdffdcbc2ae1cc8b05306249cfa6f03b755d64
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-06-06 18:29:51 +0200

    Solution

---
+++ LeetCode/Sales Analysis III/Sales Analysis III.sql
@@ -0,0 +1,6 @@
+
+select product_id , product_name
+from Product 
+where product_id not in (select product_id 
+                        from Sales 
+                        where sale_date < '2019-01-01' or sale_date > '2019-03-31')
