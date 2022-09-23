6ed6d999ec0336f156d5cc963d407a22e31675c4
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-09-23 09:03:50 +0200

    Solution

---
+++ Interview Question/Twitter Problems/Tweets' Rolling Averages/Solution.sql
@@ -0,0 +1,8 @@
+SELECT 
+  user_id,
+  tweet_date,
+  ROUND(AVG(COUNT(tweet_id)) 
+        OVER(PARTITION BY user_id ORDER BY tweet_date  ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) AS rolling_avg_3days
+FROM 
+  tweets
+GROUP BY user_id, tweet_date
